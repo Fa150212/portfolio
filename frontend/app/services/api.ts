3a7fd0624e4af650ae7561445c48c2c9cd397bc4
antaseck27 <antalissa10@gmail.com>
2026-03-10 14:28:22 +0100

    tjr blmm

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,1 +1,7 @@
-export const API_URL = "https://portfolio-1-5hfg.onrender.com/api";
+// export const API_URL = "https://portfolio-1-5hfg.onrender.com/api";
+
+
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://portfolio-1-5hfg.onrender.com/api"
+    : "http://localhost:5000/api";
